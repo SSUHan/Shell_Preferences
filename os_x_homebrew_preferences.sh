5ce46ee31246533ddbf3589cea72020cafa607e2
이준수 <ljs93kr@gmail.com>
2016-09-11 14:48:17 +0900

    os x install script

---
+++ os_x_homebrew_preferences.sh
@@ -0,0 +1,15 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install vim
+brew install vim
+
+# install git
+brew install git
+
+# install python3
+brew install python3
+
+# install autoenv
+brew install autoenv
+echo 'source /usr/local/opt/autoenv/activate.sh' >> ~/.bash_profile
