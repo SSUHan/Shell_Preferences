717205090378791210a5822bd23cd5949041b126
Ubuntu <ubuntu@ip-172-31-35-225.ec2.internal>
2016-09-23 13:39:25 +0000

    pip3 install and install virtualenv, this would be worked well by -p python3 also

--- ubuntu_aptget_preferences.sh
+++ ubuntu_aptget_preferences.sh
@@ -6,3 +6,7 @@ sudo apt-get install git -y
 
 # install python, python3, pip
 sudo apt-get install python python-dev python-pip -y
+sudo apt-get install python3-pip -y
+
+# install virtualenv
+sudo pip install virtualenv
